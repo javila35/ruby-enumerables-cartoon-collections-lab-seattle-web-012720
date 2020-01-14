dd1ae38ea98690416506c05687f30bbb9016035f
Joseph Avila <josepharthuravila@gmail.com>
2020-01-14 00:34:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def long_planeteer_calls(array_of_calls)
 end
 
 def find_the_cheese(array_of_strings)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_strings.include? 
 end
