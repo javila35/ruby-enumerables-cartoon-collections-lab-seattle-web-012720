cd487f0bc7a7034ac2f24d0d4bb5a36c9b9767c3
Joseph Avila <josepharthuravila@gmail.com>
2020-01-14 00:35:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 
 def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_strings.include? (cheese_types)
+  puts array_of_strings.include? (cheese_types)
 end
