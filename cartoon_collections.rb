133455e58f1c99c5c07c1fc7155ce7fbdadb5499
Joseph Avila <josepharthuravila@gmail.com>
2020-01-14 00:29:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array_of_calls)
-  array_of_calls.find { |word| word.length <=4 }
+  array_of_calls.find { |word| word.length >=4 }
 end
 
 def find_the_cheese# code an argument here
